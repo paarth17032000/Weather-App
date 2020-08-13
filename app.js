abe9cc9ac337916c81275a0bd1b4a010e87cf473
Paarth Agarwal <56001117+paarth17032000@users.noreply.github.com>
2020-08-14 00:14:05 +0530

    setting api properties and updating js

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
 window.addEventListener('load', () => {
-    const cityName = document.querySelector('#city-name');
+//     DOM content selected 
+     const cityName = document.querySelector('#city-name');
     const countryCode = document.querySelector('#country-code');
     const temp = document.querySelector('#temp');
     const description = document.querySelector('#description');
-    const text = document.querySelector('.display');
     let lat;
     let long;
-
+// navigator.geolocation , a function of js helps to know longitude and latitude of user's location  
+// location should be enabled by the user
     if(navigator.geolocation.getCurrentPosition(position => {
-        console.log(position);
+// grabing latitude's and longitude's coordinates to pass in the api 
         lat = position.coords.latitude;
         long = position.coords.longitude;
+// help to reach api's url
         const query = 'https://cors-anywhere.herokuapp.com/';
-
+// function defined
         async function weather(){
-            // let req = await fetch(`${query}api.openweathermap.org/data/2.5/weather?lat=28.627926&lon=79.804176&appid=865f8cbe25935bdf57994df98209420b`);
+// fetching api by passing in latitude and longitude and making a request
+// This api is taken from openweather.org and it is free
             let req = await fetch(`${query}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=865f8cbe25935bdf57994df98209420b`);
+// Converting delivered request into JSON
             let res = await req.json();
-            let data = res.data;
-            console.log(data);
+// using data from api to deliver the desired output to user
+            cityName.textContent = res.name;
+            countryCode.textContent = res.sys.country;
+            temp.innerHTML = `${res.main.temp}<sup>&#8451;</sup>`;
+            description.textContent = res.weather[0].main;
         }
-
+// function called
         weather();
     }));
 })
